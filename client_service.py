ca2510292321d546d2c7ed0404055950e3329b64
Regenal Grant <regenalgrant@users.noreply.github.com>
2020-05-09 21:28:38 -0700

    Updating Client Service
    adding commands for client service

--- client_service.py
+++ client_service.py
@@ -2,3 +2,19 @@
 import rospy
 from ROS_py.srv import WordCount
 import sys
+
+
+rospy.init_node('service_client')
+
+""" Registration """
+rospy.wait_for_service('word_count') 
+
+"""Creating proxy """
+word_counter = rospy.ServiceProxy( 
+
+# Service Name
+	'word_count', 
+
+# Service Type
+  WordCount     
+)
